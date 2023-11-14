2cda1d3520b39b251666885357e0821ffd022d1d
Imranhassan1098 <146831808+Imranhassan1098@users.noreply.github.com>
2023-11-14 12:10:03 +0000

    Initial commit

---
+++ Assets/PlayerTeleporter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTeleporter : MonoBehaviour
+{
+    private GameObject currentTeleporter;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void onTrigg
+    {
+
+    }
+}
